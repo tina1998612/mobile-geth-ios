c13250df32d66e241459bb0b16cde2228d59bba9
Tina Lee <tleeae@connect.ust.hk>
2018-05-06 15:14:40 +0800

    Initial Commit

---
+++ mobile-geth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobile-geth
+//
+//  Created by tina on 6/5/2018.
+//  Copyright © 2018 tina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
